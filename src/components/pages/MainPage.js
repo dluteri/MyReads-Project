36be607ac0366045dfb6731780fcfb2d178e31b6
Donna Luteri <donna.luteri@gmail.com>
2018-10-09 20:24:04 -0500

    feat: Link to search page button

--- src/components/pages/MainPage.js
+++ src/components/pages/MainPage.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
+
 
 class MainPage extends React.Component {
     render() {
@@ -159,7 +161,7 @@ class MainPage extends React.Component {
               </div>
             </div>
             <div className="open-search">
-              <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
+              <Link to="/search">Add a book</Link>
             </div>
           </div>
         );
