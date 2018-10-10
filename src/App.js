7aa4bd89bb32a81d08cab9fe1172008702f16c3e
Donna Luteri <donna.luteri@gmail.com>
2018-10-10 13:57:51 -0500

    fix: Shelf is showing appropriately

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react'
-// import * as BooksAPI from './BooksAPI'
 import './App.css'
 
 import { Route } from 'react-router-dom'
@@ -29,4 +28,4 @@ class BooksApp extends React.Component {
   }
 }
 
-export default BooksApp
+export default BooksApp;
