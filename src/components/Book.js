cf5b0fb8e2735d5114d95ecaf56328ddf1ce3000
Donna Luteri <donna.luteri@gmail.com>
2018-10-12 12:10:31 -0500

    feat: Add search component

--- src/components/Book.js
+++ src/components/Book.js
@@ -10,13 +10,6 @@ class Book extends React.Component {
     }
   }
 
-
-  componentDidMount() {
-    console.log(this);
-  }
-
-
-
     render() {
         return (
             <li>
@@ -25,7 +18,7 @@ class Book extends React.Component {
                 {/*} TODO - add alt tags to images */}
                 <div className="book-cover" style={{ width: 128, height: 185, backgroundImage: `url("${this.props.book.imageLinks && this.props.book.imageLinks.thumbnail || ""}")` }}></div>
                 <div className="book-shelf-changer">
-                  <select value={this.state.book.shelf || "none"} onChange={(event) => { this.props.updateBook(this.props.book, event.target.value) }}>
+                  <select value={this.props.book.shelf || "none"} onChange={(event) => { this.props.updateBook(this.props.book, event.target.value) }}>
                     <option value="move" disabled>Move to...</option>
                     <option value="currentlyReading">Currently Reading</option>
                     <option value="wantToRead">Want to Read</option>
@@ -34,8 +27,8 @@ class Book extends React.Component {
                   </select>
                 </div>
               </div>
-              <div className="book-title">{this.state.book.title}</div>
-              <div className="book-authors">{this.state.book.authors.join(', ') || "No Author..."  }</div>
+              <div className="book-title">{this.props.book.title}</div>
+              <div className="book-authors">{this.props.book.authors.join(', ') && this.props.book.authors[0]  || "No Author..."  }</div>
             </div>
           </li>
         );
